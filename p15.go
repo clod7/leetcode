c4e72c38e7674958ebe76a822b517c5630c1f516
ltm <luotianmai@163.com>
2020-12-12 10:21:16 +0800

    update

--- p15.go
+++ p15.go
@@ -11,13 +11,24 @@ func threeSum(nums []int) [][]int {
 	res := make([][]int, 0)
 
 	for k := 0; k < len(nums); k++ {
+		if k > 0 && nums[k] == nums[k-1] {
+			continue
+		}
+
 		i, j := k+1, len(nums)-1
 		for i < j {
 			if nums[i]+nums[j]+nums[k] < 0 {
 				i++
 			} else if nums[i]+nums[j]+nums[k] == 0 {
 				res = append(res, []int{nums[k], nums[i], nums[j]})
-				break
+				for i < j && nums[i] == nums[i+1] {
+					i++
+				}
+				for i < j && nums[j] == nums[j-1] {
+					j--
+				}
+				i++
+				j--
 			} else {
 				j--
 			}
